52872d7a38a653c563de29bf9a9e7e950509e226
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-21 12:15:56 +0300

    Output new change

--- Output.java
+++ Output.java
@@ -1,9 +1,16 @@
 public class Output {
-  public static void main(String[ ] args) {
-    for(int i = 0;i <= 100;i ++) {
-      if(i % 3 == 0) {
-        System.out.println("3*" + i/3);
-      } else { 
+  public static void main(String[] args) {
+    for (int i = 0; i <= 100; i++) {
+      if(i % 15 == 0) {
+        System.out.println("Tutti-Frutti");
+      }
+      else if(i % 5 == 0) {
+        System.out.println("Frutti");
+      }
+      else if(i % 3 == 0) {
+        System.out.println("Tutti");
+      }
+      else {
         System.out.println(i);
       }
     }
