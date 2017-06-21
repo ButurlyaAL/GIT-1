9b8aa90e9d937b25eb0fc5e23c73e93f309e758d
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-21 15:47:28 +0300

    Update Output.java

--- Output.java
+++ Output.java
@@ -3,14 +3,11 @@ public class Output {
     for (int i = 0; i <= 100; i++) {
       if (i % 15 == 0) {
         System.out.println("Tutti-Frutti");
-      }
-      else if (i % 5 == 0) {
+      } else if (i % 5 == 0) {
         System.out.println("Frutti");
-      }
-      else if (i % 3 == 0) {
+      } else if (i % 3 == 0) {
         System.out.println("Tutti");
-      }
-      else {
+      } else {
         System.out.println(i);
       }
     }
