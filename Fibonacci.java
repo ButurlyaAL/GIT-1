8990827dd3e07598e45ab4dd873ed407a2cfdf24
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-23 09:45:20 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,7 +6,7 @@ import java.util.InputMismatchException;
       try{
         Scanner input = new Scanner(System.in);
         double number=input.nextDouble(); 
-        double number_sqrt_1=Math.sqrt(number*number*5-4),number_sqrt_2=Math.sqrt(number*number*5+4);
+        double number_sqrt_1=Math.sqrt(number * number *5 - 4),number_sqrt_2=Math.sqrt(number * number *5 + 4);
         if (number_sqrt_1 % 1 == 0 || number_sqrt_2 % 1 == 0) {
           System.out.println("It is Fibonacci number");
         } else {
