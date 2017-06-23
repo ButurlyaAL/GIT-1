6b89870fa513bf196adeb5c6a3cef0901500e2cd
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-24 00:12:45 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,7 +3,7 @@ public class Fibonacci {
   public static void main(String[] args) { 
     System.out.println("Enter an integer : "); 
     Scanner input = new Scanner(System.in);
-    double number = input.nextDouble(); 
+    int number = input.nextInt(); 
     double number_sqrt_1 = Math.sqrt(number * number * 5 - 4),number_sqrt_2 = Math.sqrt(number * number * 5 + 4);
     if ((number_sqrt_1 % 1 == 0 || number_sqrt_2 % 1 == 0) && number >= 0)  {
       System.out.println("It is non-negative Fibonacci number");
