66373d99439e73e9f339c03f4a07b85267dae6b9
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-23 12:44:52 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-  public class Fibonacci {
-    public static void main(String[] args) { 
-      System.out.println("Enter an integer : "); 
-        Scanner input = new Scanner(System.in);
-        double number=input.nextDouble(); 
-        double number_sqrt_1=Math.sqrt(number * number *5 - 4),number_sqrt_2=Math.sqrt(number * number *5 + 4);
-        if ((number_sqrt_1 % 1 == 0 || number_sqrt_2 % 1 == 0) && number >= 0)  {
-          System.out.println("It is Fibonacci number");
-        } else {
-          System.out.println("It is not Fibonacci number");
-        }
-      } 
+public class Fibonacci {
+  public static void main(String[] args) { 
+    System.out.println("Enter an integer : "); 
+    Scanner input = new Scanner(System.in);
+    double number=input.nextDouble(); 
+    double number_sqrt_1=Math.sqrt(number * number *5 - 4),number_sqrt_2=Math.sqrt(number * number *5 + 4);
+    if ((number_sqrt_1 % 1 == 0 || number_sqrt_2 % 1 == 0) && number >= 0)  {
+      System.out.println("It is Fibonacci number");
+    } else {
+      System.out.println("It is not Fibonacci number");
     }
+  } 
+}
