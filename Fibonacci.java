bdc8059c66b7e043b30dcfde44e04b296972f88c
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-24 09:48:43 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
+
 public class Fibonacci {
   public static void main(String[] args) { 
-    System.out.println("Enter an integer : "); 
     Scanner input = new Scanner(System.in);
+    System.out.println("Enter an integer : "); 
     int number = input.nextInt(); 
-    double number_sqrt_1 = Math.sqrt(number * number * 5 - 4),number_sqrt_2 = Math.sqrt(number * number * 5 + 4);
-    if ((number_sqrt_1 % 1 == 0 || number_sqrt_2 % 1 == 0) && number >= 0)  {
+    double firstNumberSqrt = Math.sqrt(number * number * 5 - 4);
+    double secondNumberSqrt = Math.sqrt(number * number * 5 + 4);
+    if ((firstNumberSqrt % 1 == 0 || secondNumberSqrt % 1 == 0) && number >= 0) {
       System.out.println("It is non-negative Fibonacci number");
     } else {
       System.out.println("It is not non-negative Fibonacci number");
