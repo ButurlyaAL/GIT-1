597f1ad0797d68248179cb75c00f82936e53f191
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-24 15:58:13 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -6,6 +6,8 @@ public class Fibonacci {
     Scanner input = new Scanner(System.in);
     int number = input.nextInt();
     while(number < 0) {
+      /*if the number is negative
+        request to enter again*/ 
       System.out.println("Wrong!!!(Your number is negative)Try again :");
       number = input.nextInt();
     }
@@ -15,14 +17,19 @@ public class Fibonacci {
     int nextCount = 1;
     while(iteration < n) {
       if(currentCount == number) {
+        /*if we have concidence:
+          it is Fibonacci number*/
         System.out.println("It is non-negative Fibonacci number");
         break;
       }
+      //calculation
       nextCount += currentCount;
       currentCount = nextCount - currentCount;
      
 
       if(iteration == n - 1 && currentCount != number) {
+        /*if we have reached the end of the cicle and there is no coincidence
+          decide that the number entered is not a Fibonacci number*/
         System.out.println("It is not non-negative Fibonacci number");
       }
       iteration ++;
