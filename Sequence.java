62f9da54cf2714d68238aa5552bf09f0034e34b4
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-26 01:50:46 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Sequence {
   public static void main(String[] args) { 
     Scanner input = new Scanner(System.in);
-    System.out.println("Enter an integer sequence(If you have finished ,press any letter) : ");
-    ArrayList<Integer> sequence = new ArrayList<Integer>();
-    while (input.hasNextInt()) {
-      sequence.add(input.nextInt());    
-    }
-    for (int index = 0; index < sequence.size() - 1; index ++) {                 
-      if (sequence.get(index + 1) < sequence.get(index)) {
-        System.out.println("It is NOT non-decreasing sequence");
+    System.out.println("Enter an integer sequence(without blancs : ");
+    String sequence = input.nextLine();
+    int currentElement;
+    int nextElement;
+    for (int index = 0; index < sequence.length() - 1; index++) {
+      currentElement = Integer.valueOf(sequence.charAt(index));
+      nextElement = Integer.valueOf(sequence.charAt(index + 1));
+      if (nextElement < currentElement) {
+        System.out.println("It is NOT non-decreasung sequence");
         break;
-      } else if (index == sequence.size() -2) {
-        System.out.println("It is non-decreasing sequence");
-      }
-    }
+      } else if (index == sequence.length() - 2) {
+        System.out.println("It is non-decreasung sequence");
+      }     
+    }  
   }
 }
