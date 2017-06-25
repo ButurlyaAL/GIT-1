c8ff17c52fc2a5da5de23c89242208c4ce8ec137
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-26 01:52:08 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -7,7 +7,7 @@ public class Sequence {
     String sequence = input.nextLine();
     int currentElement;
     int nextElement;
-    for (int index = 0; index < sequence.length() - 1; index++) {
+    for (int index = 0; index < sequence.length() - 1; index ++) {
       currentElement = Integer.valueOf(sequence.charAt(index));
       nextElement = Integer.valueOf(sequence.charAt(index + 1));
       if (nextElement < currentElement) {
