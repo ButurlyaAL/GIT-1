b639166a5c9ee883e04eb92f15425ffe94519f32
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-25 17:25:43 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -6,26 +6,19 @@ public class Sequence {
     System.out.println("Enter an integer sequence(If you have finished ,press any letter) : ");
     ArrayList<Integer> sequence = new ArrayList<Integer>();
     Scanner input = new Scanner(System.in);
-    while(true) {
+    while (true) {
       if (input.hasNextInt() == false) {
         break;
       }
       sequence.add(input.nextInt());    
     }
     for (int element = 0; element < sequence.size() - 1; element ++) {                 
-      if(sequence.get(element + 1) < sequence.get(element)) {
+      if (sequence.get(element + 1) < sequence.get(element)) {
         System.out.println("It is not non-decreasing sequence");
         break;
-      } else if(element == sequence.size() -2) {
+      } else if (element == sequence.size() -2) {
         System.out.println("It is non-decreasing sequence");
       }
     }
   }
 }
-                        
-
-   
-     
-     
-           
-      
