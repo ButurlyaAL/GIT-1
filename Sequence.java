548e6a7f49ee4532e57f54d5f0ba67b81b6c99e4
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-26 17:15:13 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Sequence {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    System.out.println("Enter an INTEGER sequence(use spaces(If you have finished, please press ENTER)) : ");
+    System.out.println("Enter an INTEGER sequence(use spaces(If you have finished, please press ENTER and any letter)) : ");
     int currentElement = 0;
     int previousElement = 0;
     while (input.hasNextInt()) {
