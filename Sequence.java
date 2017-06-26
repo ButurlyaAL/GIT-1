1ff92cfaed2c1c0c61049a897283297f813814b0
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-26 16:00:30 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -3,21 +3,22 @@ import java.util.Scanner;
 public class Sequence {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    System.out.println("Enter an INTEGER sequence(If you have finished, please press any letter) : ");
-    int check = 0;
-    int currentElement = 0;
-    int previousElement = 0;
-    while (input.hasNextInt()) {
+    System.out.println("Enter an INTEGER sequence(use spaces(If you have finished, please press ENTER)) : ");
+    int currentNum = 0;
+    int previousNum = 0;
+	  String result = "";
+	  while (input.hasNextInt()) {
       currentNum = input.nextInt();
       if (currentElement < previousElement && previousElement != 0) {
-        check ++;
-      }
-     previousElement = currentElement;
-    }
-    if (input.hasNextLine() && check == 0) {
-      System.out.println("It is non-decreasing sequence.");
-    } else if (input.hasNextLine() && check > 0) {
-      System.out.println("it is NOT non-decreasing sequence");
-    }
+        result = "It is NOT non-decreasing sequence.";
+        break;
+       } else {
+         previousElement = currentElement;
+       }
+       if (input.hasNextLine()) {
+		     result = "It is non-decreasing sequence.";
+         break;
+       }
+     }
+	 System.out.println(result);
   }
-}
