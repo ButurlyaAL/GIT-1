31ab2da7bb4ad13eec2fd424a3b90393a58db151
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-26 17:14:11 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -4,8 +4,8 @@ public class Sequence {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.println("Enter an INTEGER sequence(use spaces(If you have finished, please press ENTER)) : ");
-    int currentNum = 0;
-    int previousNum = 0;
+    int currentElement = 0;
+    int previousElement = 0;
     while (input.hasNextInt()) {
       currentElement = input.nextInt();
       if (currentElement < previousElement && previousElement != 0) {
@@ -14,8 +14,7 @@ public class Sequence {
       } else {
         previousElement = currentElement;
       }
-      System.out.println("It is non-decreasing sequence.");
-      }
     }
-    System.out.println(result);
+    System.out.println("It is non-decreasing sequence.");
+  }
 }
