1ecb77d975f7fe81d60f1703504f2b1e39d7e706
ButurlyaAL <stdlibh1421@gmail.com>
2017-06-27 16:57:18 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -6,6 +6,12 @@ public class Sequence {
     System.out.println("Enter an INTEGER sequence(use spaces(If you have finished,press ENTER)):");
     int currentElement = 0;
     int previousElement = 0;
+    try {
+      previousNum = input.nextInt();
+    } catch (Exception e) {
+      System.out.println("It's NOT integer value. Please try again.");
+      return;
+    }
     while (input.hasNextInt()) {
       currentElement = input.nextInt();
       if (currentElement < previousElement && previousElement != 0) {
