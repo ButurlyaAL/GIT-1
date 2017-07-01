be6841880c8e2b3d7c42e1f5f2973550b43abc42
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-01 11:38:18 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -3,37 +3,22 @@ import java.util.Scanner;
 public class Sequence {
   public static void main(String[] args) {
     if (args.length == 0) {
-      consoleInput();  
-    } else {  
-      for (int i = 0; i < args.length - 1; i ++) {
-        if (Integer.valueOf(args[i + 1]) < Integer.valueOf(args[i])) {
-          System.out.println("It is NOT non-decreasing sequence.");
-          return;
-        }
-      }
-      System.out.println("It is non-decreasing sequence.");               
+      Scanner input = new Scanner(System.in);
+      System.out.println("Enter an INTEGER sequence(use spaces(If you have finished,press ENTER)):");
+      String line = input.nextLine();
+      String[] sequence = line.split(" ");
+      checking(sequence);
+    } else { 
+      checking(args);
     }
   }
-  public static void consoleInput() {
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter an INTEGER sequence(use spaces(If you have finished,press ENTER)):");
-    int currentElement = 0;
-    int previousElement = 0;
-    try {
-      previousElement = input.nextInt();
-    } catch (Exception e) {
-      System.out.println("It's NOT integer value. Please try again.");
-      return;
-    }
-    while (input.hasNextInt()) {
-      currentElement = input.nextInt();
-      if (currentElement < previousElement) {
+  public static void checking(String[] numbers) { 
+    for (int i = 0; i < numbers.length - 1; i ++) {
+      if (Integer.valueOf(numbers[i + 1]) < Integer.valueOf(numbers[i])) {
         System.out.println("It is NOT non-decreasing sequence.");
         return;
-      } else {
-        previousElement = currentElement;
       }
     }
-    System.out.println("It is non-decreasing sequence.");
+    System.out.println("It is non-decreasing sequence.");               
   }
-}      
+}
