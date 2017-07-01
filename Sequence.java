f0952dd22112619ba50af91564a0978302578de2
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-01 21:37:32 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -3,14 +3,14 @@ public class Sequence {
   public static void main(String[] args) {
     Checker checker = new Checker();
     /** A boolean value thats determines the type of sequence:non-decreasing or not. */
-    boolean check;
+    boolean resultOfCheck;
     if (args.length == 0) {
       ConsoleReader consoleReader = new ConsoleReader();
-      check = checker.checking(consoleReader.getSequence());
+      resultOfCheck = checker.check(consoleReader.getSequence());
     } else { 
-      check = checker.checking(args);  
+      resultOfCheck = checker.check(args);  
     }
-    if (check) {
+    if (resultOfCheck) {
     System.out.println("It is NOT non-decreasing sequence.");
     } else {
       System.out.println("It is non-decreasing sequence.");
