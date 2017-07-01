c2612f15375b7d3c160bfcfcb90fd08ae271edf8
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-01 21:54:46 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -1,8 +1,12 @@
-/** Print the type of sequence:non decreasing or not. */
+/** 
+ *Print the type of sequence:non decreasing or not. 
+ */
 public class Sequence {
   public static void main(String[] args) {
     Checker checker = new Checker();
-    /** A boolean value thats determines the type of sequence:non-decreasing or not. */
+    /** 
+     *A boolean value thats determines the type of sequence:non-decreasing or not. 
+     */
     boolean resultOfCheck;
     if (args.length == 0) {
       ConsoleReader consoleReader = new ConsoleReader();
