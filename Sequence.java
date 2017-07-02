42178a6398c33f824270c21408bc7a9a8eb4f6ad
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-03 00:50:22 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -4,10 +4,6 @@
 public class Sequence {
   public static void main(String[] args) {
     Checker checker = new Checker();
-/** 
- *A boolean value thats determines the type of sequence:non-decreasing or not. 
- */
-    boolean resultOfCheck;
     if (args.length == 0) {
       ConsoleReader consoleReader = new ConsoleReader();
       checker.printResultOfCheck(checker.check(consoleReader.getSequence()));
