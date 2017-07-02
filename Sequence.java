079797386ba195f3df987fa744c033da7198e331
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-03 00:44:43 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class Sequence {
   public static void main(String[] args) {
     if (args.length != 0) { 
-      printResult(check(args));
+      printResultOfCheck(check(args));
     } else {
       Scanner input = new Scanner(System.in);
       System.out.println("Enter an INTEGER sequence(use spaces(If you have finished,press ENTER)):");
       String line = input.nextLine();
       String[] sequence = line.split(" ");
-      printResult(check(sequence));   
+      printResultOfCheck(check(sequence));   
     }
   }
 /**
@@ -30,7 +30,7 @@ public class Sequence {
   *Prints what sequence is.
   *@resultOfCheck is boolean value,which is the result of verification of sequence.
   */
-  public static void printResult(boolean resultOfCheck) {
+  public static void printResultOfCheck(boolean resultOfCheck) {
      if (resultOfCheck) {
     System.out.println("It is NOT non-decreasing sequence.");
     } else {
