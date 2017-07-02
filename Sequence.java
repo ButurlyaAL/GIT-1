f70ed3e04a60ae5c6da39c60836b6ae0d9262827
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-03 00:49:53 +0300

    Update Sequence.java

--- Sequence.java
+++ Sequence.java
@@ -4,15 +4,15 @@
 public class Sequence {
   public static void main(String[] args) {
     Checker checker = new Checker();
-    /** 
-     *A boolean value thats determines the type of sequence:non-decreasing or not. 
-     */
+/** 
+ *A boolean value thats determines the type of sequence:non-decreasing or not. 
+ */
     boolean resultOfCheck;
     if (args.length == 0) {
       ConsoleReader consoleReader = new ConsoleReader();
-      resultOfCheck = checker.check(consoleReader.getSequence());
+      checker.printResultOfCheck(checker.check(consoleReader.getSequence()));
     } else { 
-      resultOfCheck = checker.check(args);  
+      checker.printResultOfCheck(checker.check(args));  
     }
     if (resultOfCheck) {
     System.out.println("It is NOT non-decreasing sequence.");
