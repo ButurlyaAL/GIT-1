55138261d7bb5ce3c4ee8058f77a5753fa6b1dae
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-03 00:47:55 +0300

    Update Checker.java

--- Checker.java
+++ Checker.java
@@ -2,12 +2,12 @@
  *Checks the type of any sequence.
  */
 public class Checker {
-  /**
-   *Returns a boolean value,
-   *which determines that the sequence is non-decreasing or not.
-   *@numbers is the sequence to be checked
-   *@return boolean result of check
-   */
+/**
+ *Returns a boolean value,
+ *which determines that the sequence is non-decreasing or not.
+ *@numbers is the sequence to be checked
+ *@return boolean result of check
+ */
   public boolean check(String[] sequence) {
     for (int i = 0; i < sequence.length - 1; i ++) {
       if (Integer.valueOf(sequence[i + 1]) < Integer.valueOf(sequence[i])) {
@@ -16,4 +16,10 @@ public class Checker {
     }
     return false;               
   }
+  public void printReultOfCheck(boolean resultOfCheck) {
+    if (resultOfCheck) {
+      System.out.println("It is NOT non-decreasing sequence.");
+    } else {
+      System.out.println("It is non-decreasing sequence.");
+    }
 }    
