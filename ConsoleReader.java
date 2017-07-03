237377326cc12875bb5719f8874b655feb2c7198
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-04 01:26:01 +0300

    Update ConsoleReader.java

--- ConsoleReader.java
+++ ConsoleReader.java
@@ -1,25 +1,19 @@
 import java.util.Scanner;
+
 /**
- *Read a sequence from the console.
+ * Read a sequence from the console.
  */
 public class ConsoleReader {
-  private Scanner input;
-  private String line;
 /**
- *Class constructor
- */
-  public ConsoleReader() {
-    input = new Scanner(System.in);
-    System.out.println("Enter an INTEGER sequence(use spaces(If you have finished,press ENTER)):");
-    line = input.nextLine();
-  }
-/**
- *Returns String array,
- *which consists of the numbers(in the string representation) entered by the user
- *@return String array
+ * Returns String array,
+ * which consists of the numbers(in the string representation) entered by the user
+ * @return String array
  */
   public String[] getSequence() {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Enter an INTEGER sequence(use spaces(If you have finished,press ENTER)):");
+    String line = input.nextLine();
     String[] sequence = line.split(" ");
     return sequence;
-  }
+  } 
 }  
