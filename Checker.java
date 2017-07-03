ac0cb9ed9e96a7ca694161b608a7d616bc73cfaa
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-04 01:31:21 +0300

    Update Checker.java

--- Checker.java
+++ Checker.java
@@ -6,7 +6,7 @@ public class Checker {
  * Returns a boolean value,
  * which determines that the sequence is non-decreasing or not.
  * @param sequence is the sequence to be checked
- * @return a boolean value which is tue when sequence is non-decreasing or false when is not.
+ * @return a boolean value which is true when sequence is non-decreasing or false when is not.
  */
   public boolean check(String[] sequence) {
     for (int i = 0; i < sequence.length - 1; i ++) {
@@ -16,4 +16,15 @@ public class Checker {
     }
     return false;               
   }
-}    
+/**
+ * Prints what sequence is.
+ * @param resultOfCheck a boolean value,which determines what to print.
+ */
+public static void printReultOfCheck(boolean resultOfCheck) {
+    if (resultOfCheck) {
+      System.out.println("It is NOT non-decreasing sequence.");
+    } else {
+      System.out.println("It is non-decreasing sequence.");
+    }
+  }
+}
