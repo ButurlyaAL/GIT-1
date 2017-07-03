f9a6656d6acc12b399fc4297ad07c5a1ea51a4b1
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-04 01:42:11 +0300

    Update ConsoleReader.java

--- ConsoleReader.java
+++ ConsoleReader.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
  * Read a sequence from the console.
  */
 public class ConsoleReader {
-/**
- * Returns String array,
- * which consists of the numbers(in the string representation) entered by the user
- * @return String array
- */
+  /**
+   * Returns String array,
+   * which consists of the numbers(in the string representation) entered by the user
+   * @return String array
+   */
   public String[] getSequence() {
     Scanner input = new Scanner(System.in);
     System.out.println("Enter an INTEGER sequence(use spaces(If you have finished,press ENTER)):");
