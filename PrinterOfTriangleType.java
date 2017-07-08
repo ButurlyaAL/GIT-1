b7a252958c442c94193f92235b2adb384935b00c
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-08 18:25:23 +0300

    add javadoc

---
+++ PrinterOfTriangleType.java
@@ -0,0 +1,18 @@
+/** 
+ * Prints the type of triangle:normal,isosceles and equilateral. 
+ */
+public class PrinterOfTriangleType {
+  /**
+   * Prints the type of triangle.
+   * @param qualifier is a integer value,which determines what to print.
+   */
+  public void printTypeOfTriangle(int qualifier) {
+    if(qualifier == 0) {
+      System.out.println("Triangle is normal.");
+    } else if (qualifier == 1) {
+      System.out.println("Triangle is isosceles.");
+    } else if (qualifier == 2) {
+      System.out.println("Triangle is equilateral.");
+    }
+  }
+}
