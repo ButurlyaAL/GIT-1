77a8065343202a0cb45c455ae23905fac436759f
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-08 18:24:16 +0300

    add javadoc

--- TriangleConsoleReader.java
+++ TriangleConsoleReader.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
 
-public class TringleConsoleReader {
+/**
+ * Read the sequence of values for the sides of the triangle.
+ */
+public class TriangleConsoleReader {
+  /**
+   * Returns String array,
+   * which consists of the numbers(in the string representation) entered by the user
+   * @return String array
+   */
   public String[] getSequence() {
     Scanner input = new Scanner(System.in);
     System.out.println("Enter the value of 3 sides of the triangle(using spaces):");
