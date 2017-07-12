1eb66be96c35c50ccafb04590a1b76867d9cdddb
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-12 18:40:04 +0300

    fix indentation

--- EntryClass.java
+++ EntryClass.java
@@ -18,14 +18,14 @@ public class EntryClass {
         Person person = new Person(information);
         persons[i] = person;
       }
-      for(int j = 0; j < number; j++) {
+      for (int j = 0; j < number; j ++) {
         System.out.printf("Firstname: %s  Lastname: %s  Age: %d  \n", persons[j].getFirstName(), persons[j].getLastName(), persons[j].getAge());
       } 
-    } catch(NumberFormatException a) {
+    } catch (NumberFormatException a) {
       System.out.println("You have entered age in wrong format(Age must be POSITIVE and INTEGER.Start programm again.");
-    } catch(IllegalArgumentException b) {
+    } catch (IllegalArgumentException b) {
       System.out.println("You have entered firstName or lastName as NUMBER.Please Start programm again.");
-    } catch(ArrayIndexOutOfBoundsException c) {
+    } catch (ArrayIndexOutOfBoundsException c) {
       System.out.println("You did not complete all the fields about some persons.Start programm again.");
     }
   }
