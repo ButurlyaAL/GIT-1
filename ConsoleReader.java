d26d9e37322a998a2685161623712df8b58745a2
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-14 12:12:47 +0300

    fix identation and rename methods

--- ConsoleReader.java
+++ ConsoleReader.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
  * Read a sequence from the console.
  */
 public class ConsoleReader {
+  
   /**
    * Returns String array,
    * which consists of the numbers(in the string representation) entered by the user.
    * @return String array.
    */
-  public String[] getSequence() {
+  public String[] readSequence() {
     Scanner input = new Scanner(System.in);
     System.out.println("Enter an INTEGER sequence(use spaces(If you have finished,press ENTER)):");
     String line = input.nextLine();
