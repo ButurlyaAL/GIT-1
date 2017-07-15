51ee71bf13a855af2c0385a5f56ba174f9bba207
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-15 17:14:02 +0300

    add PrinterOfType.java and made some changes in others

--- Sequence.java
+++ Sequence.java
@@ -3,18 +3,16 @@
  */
 public class Sequence {
   public static void main(String[] args) {
-    Sequence sequence = new Sequence();
-    Checker checker = new Checker();
     PrinterOfType printer = new PrinterOfType();
     try {
       if (args.length == 0) {
-        ConsoleReader consoleReader = new ConsoleReader();
-        sequence.printTypeOfSequence(checker.isNonDecreasing(consoleReader.readSequence()));
+        ConsoleReader reader = new ConsoleReader();
+        printer.printTypeOfSequence(new Checker(reader.readSequence()).isNonDecreasing());
       } else { 
-        sequence.printTypeOfSequence(checker.isNonDecreasing(args));  
+        printer.printTypeOfSequence(new Checker(args).isNonDecreasing());  
       }
     } catch (Exception a) {
-      System.out.println("It is not an integer.Try again to enter an integer sequence.");
+      System.out.println("It is not an integer sequence.Try again to enter an integer sequence.");
       main(args);
     }
   }
