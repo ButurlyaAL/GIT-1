75fb252a5c641efe5b695bc9cf36f4ee1d173097
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-16 12:33:31 +0300

    add javadoc

--- TriangleTask/ConsoleReader.java
+++ TriangleTask/ConsoleReader.java
@@ -1,6 +1,14 @@
 import java.util.Scanner;
-  
+
+/**
+ * Read values of the sides from the console.
+ */
 public class ConsoleReader {
+   /**
+   * Returns String array,
+   * which consists of the numbers(in the string representation) entered by the user.
+   * @return string representation of sides entered by the user.
+   */
   public String[] readSequence() {
     Scanner input = new Scanner(System.in);
     System.out.println("Enter the sides of the triangle using spaces(If you have finished,press ENTER):");
