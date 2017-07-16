5445f26e7be1b27ef167a7e35d96705f72691567
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-16 13:16:42 +0300

    add javadoc

--- TriangleTask/RegularTriangleBuilder.java
+++ TriangleTask/RegularTriangleBuilder.java
@@ -1,4 +1,11 @@
+/**
+ * Builds regular triangle.
+ */
 public class RegularTriangleBuilder {
+   /**
+   * Builds equaterial triangle.
+   * @return exemplar of successor of Triangle.
+   */
   public Triangle triangleBuild(String[] sequence) {
       Triangle triangle = new RegularTriangle(sequence);
       return triangle;
