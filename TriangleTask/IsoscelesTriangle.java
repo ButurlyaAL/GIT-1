01bd43ba0204c3a6f5d5d1ce200892a7f4d2ef2c
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-16 13:05:58 +0300

    add javadoc add some chenges in exists

--- TriangleTask/IsoscelesTriangle.java
+++ TriangleTask/IsoscelesTriangle.java
@@ -1,18 +1,25 @@
+/**
+ * Representation of isosceles triangle.
+ */
 public class IsoscelesTriangle extends Triangle {
   public IsoscelesTriangle(String[] sequence) {
     super(sequence);
     exists();
   }
+  /**
+   * Checks for exist isosceles triangle.
+   * @throw IllegalArgumentException if triangle isn't equaterial or triangle can't exists.
+   */
   public void exists() {
-    if(!(firstSide.add(secondSide).compareTo(thirdSide) == 1 && firstSide.add(thirdSide).compareTo(secondSide) == 1 && secondSide.add(thirdSide).compareTo(firstSide) == 1)) {
-      throw new IllegalArgumentException();
-    }   
+    super.exists();   
     if(!(firstSide.compareTo(secondSide) == 0 || firstSide.compareTo(thirdSide) == 0 || secondSide.compareTo(thirdSide) == 0)) {
       throw new IllegalArgumentException();  
     } 
   }
+   /**
+   * @return String containing type of triangle.
+   */
   public String getType() {
     return "Triangle is isosceles.";
   }
-} 
-  
+}  
