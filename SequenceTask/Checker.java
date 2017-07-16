acbbbeec2beda2ce8afdb0098e4e813e911018f4
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-16 16:17:50 +0300

    change javadoc

--- SequenceTask/Checker.java
+++ SequenceTask/Checker.java
@@ -9,7 +9,7 @@ public class Checker {
   /**
    * Returns true if the sequence is non-decreasing.
    * @param sequence is the sequence to be checked.
-   * @return true if the sequence is non-decreasing;false if sequence is non-decreasing.
+   * @return true if the sequence is non-decreasing;false if sequence isn't non-decreasing.
    */
   public boolean checkForNondecreasing(String[] sequence) {
     for (int i = 0; i < sequence.length - 1; i ++) {
