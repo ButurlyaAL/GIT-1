3f8a82ac63514ef24d1632f36c3552d8c30740a2
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-19 14:03:26 +0300

    constructor, javadocs

--- TriangleTask/IsoscelesTriangle.java
+++ TriangleTask/IsoscelesTriangle.java
@@ -1,25 +1,28 @@
+import java.math.BigDecimal;
+
 /**
  * Representation of isosceles triangle.
  */
 public class IsoscelesTriangle extends Triangle {
-  public IsoscelesTriangle(String[] sequence) {
-    super(sequence);
+  public IsoscelesTriangle(BigDecimal[] sides) {
+    super(sides);
     exists();
   }
+  
   /**
    * Checks for exist isosceles triangle.
-   * @throw IllegalArgumentException if triangle isn't equaterial or triangle can't exists.
+   * @throw IllegalArgumentException if triangle isn't isosceles or can't exists.
    */
-  public void exists() {
-    super.exists();   
+  public void exists() {  
     if (!(firstSide.compareTo(secondSide) == 0 || firstSide.compareTo(thirdSide) == 0 || secondSide.compareTo(thirdSide) == 0)) {
-      throw new IllegalArgumentException();  
+      throw new IllegalArgumentException("It isn't isosceles triangle.");  
     } 
   }
+  
    /**
    * @return String containing type of triangle.
    */
   public String getType() {
     return "Triangle is isosceles.";
   }
-}  
+}
