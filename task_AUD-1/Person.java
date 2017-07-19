e5e53a0c68acc3d2822fb3875a6138cc54a48ab0
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-19 10:51:47 +0300

    javadoc,ageIsPositive,namesIsNumber,exceptions.

--- task_AUD-1/Person.java
+++ task_AUD-1/Person.java
@@ -6,50 +6,34 @@ public class Person {
   private String firstName;
   private String lastName;
   private int age;
+  /**
+   * Makes an exemplar of Person if all the fields are correct.
+   */
   public Person(String[] information) {
     firstName = information[0];
     lastName = information[1];
     age = Integer.valueOf(information[2]);
     verify();
   }
-  
-  /**
-   * Checks correctness of entered information about person.
-   * @throw IllegalArgumentException if firstname or lastname was entered in wrong format.
-   * @throw NumberFormatException if age is not positive.
-   */
   private void verify() {
-    if (stringIsNumber(firstName) || stringIsNumber(lastName)) {
-      throw new IllegalArgumentException();
+    if (namesIsNumber()) {
+      throw new IllegalArgumentException("Wrong firstname or lastname");
     }
-    if (!ageIsPositive(age)) {
-      throw new NumberFormatException();
+    if (!ageIsPositive()) {
+      throw new NumberFormatException("Age isn't positive.");
     } 
   }
-  
-  /**
-   * Checks whether the entered string is a number.
-   * @return true if entered string is a number;false otherwise.
-   */
-  private boolean stringIsNumber(String word) {
+  private boolean namesIsNumber() {
     try {
-      int a = Integer.valueOf(word);
-    } catch (Exception a) {
+      int number = Integer.valueOf(firstName);
+      number = Integer.valueOf(lastName);
+    } catch (Exception b) {
      return false;
     }
     return true;
   }
-  
-  /**
-   * Checks age is positive or not.
-   * @return true if age is positive;false otherwise.
-   */
-  private boolean ageIsPositive(int age) {
-    if (age < 0) {
-      return false;
-    } else {
-      return true;
-    }
+  private boolean ageIsPositive() {
+   return age >= 0;
   }
   
   /**
