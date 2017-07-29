7bd906879bf49ffddd6b0d36d9e164f2525e194f
ButurlyaAL <stdlibh1421@gmail.com>
2017-07-29 17:36:34 +0300

    change exception handling

--- task_AUD-1/EntryClass.java
+++ task_AUD-1/EntryClass.java
@@ -8,10 +8,10 @@ public class EntryClass {
       for (Person person : reader.getListOfPersons()) {
         System.out.printf("Firstname:%s  Lastname:%s  Age:%d  \n", person.getFirstName(), person.getLastName(), person.getAge());
       }
-    } catch (NumberFormatException a) {
-      System.out.println("You have entered age in wrong format(Age must be POSITIVE and INTEGER).Start programm again.");
-    } catch (IllegalArgumentException b) {
-      System.out.println("You have entered firstName or lastName as NUMBER.Please Start programm again.");
+    } catch (NumberFormatException firstException) {
+      firstException.getMessage();
+    } catch (IllegalArgumentException secondException) {
+      secondException.getMessage();
     } catch (ArrayIndexOutOfBoundsException c) {
       System.out.println("You did not complete all the fields about some persons.Start programm again.");
     }
