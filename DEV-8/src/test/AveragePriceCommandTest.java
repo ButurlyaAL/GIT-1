3b035243476e9e21432d1ad5122511287dbfdcae
ButurlyaAL <stdlibh1421@gmail.com>
2017-08-16 00:53:54 +0300

    fix indentation

--- DEV-8/src/test/AveragePriceCommandTest.java
+++ DEV-8/src/test/AveragePriceCommandTest.java
@@ -1,10 +1,9 @@
 import org.junit.Test;
-
 import java.math.BigDecimal;
-
 import static org.junit.Assert.*;
 
 public class AveragePriceCommandTest {
+  
   @Test
   public void execute() throws Exception {
     DataBase dataBase = new DataBase();
@@ -13,7 +12,5 @@ public class AveragePriceCommandTest {
     dataBase.add(new Product("secondType", "name", 1, new BigDecimal("1.0")));
     dataBase.add(new Product("thirdType", "name", 4, new BigDecimal("3.0")));
     assertTrue(new BigDecimal("2.1").compareTo(new AveragePriceCommand(dataBase).averagePrice()) == 0);
-
   }
-
 }
