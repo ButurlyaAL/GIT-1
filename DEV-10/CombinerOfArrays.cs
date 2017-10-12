0bc66607bdc6ac3ba9ef8c37102a401f6be6c2cc
ButurlyaAL <stdlibh1421@gmail.com>
2017-10-13 00:43:25 +0300

    task_DEV-10

---
+++ DEV-10/CombinerOfArrays.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace DEV_10
+{
+
+  /// <summary>
+  /// Combines arrays.
+  /// </summary>
+  class CombinerOfArrays
+  {
+    private Sorter sorter;
+
+    /// <summary>
+    /// Creates combiner of arrays.
+    /// </summary>
+    public CombinerOfArrays()
+    {
+      sorter = new Sorter();
+    }
+
+    /// <summary>
+    /// Returns array with elements wich repeated more than once in different arrays.
+    /// </summary>
+    /// <param name="listOfArrays">Arrays to combine.</param>
+    /// <returns>Returns array with elements wich repeated more than twice.</returns>
+    public double[] GetUnionOfArrays(List<double[]> listOfArrays)
+    {
+      List<double> result = new List<double>();
+      for (int i = 0; i < listOfArrays.Count; i++)
+      {
+        listOfArrays[i] = this.GetSortedArrayWhithoutRepeat(listOfArrays[i]);
+        result.AddRange(listOfArrays[i]);
+      }
+      return this.GetRepeatedElements(sorter.MergeSort(result).ToArray<double>());
+    }
+
+    /// <summary>
+    /// Returns array with elements wich repeated more than once in the array.
+    /// </summary>
+    /// <param name="array">Source array.</param>
+    /// <returns>Array with elements wich repeated more than once in the array.</returns>
+    public double[] GetRepeatedElements(double[] array)
+    {
+       List<double> result = new List<double>();
+       if (array.Length == 1)
+       {
+         return array;                                       
+       }
+       for (int i = 0; i < array.Length - 1; i ++)
+       {                
+         if(this.CompareDoubles(array[i], array[i + 1]) && !result.Contains(array[i]))
+         {
+           result.Add(array[i]);     
+         }
+       }
+       if (this.CompareDoubles(array[array.Length - 1],array[array.Length - 2]) && !result.Contains(array[array.Length - 1]))
+       {
+         result.Add(array[array.Length - 1]);
+       }
+       return result.ToArray<double>();
+     }
+    
+    /// <summary>
+    /// Returns array without repeated elements.
+    /// </summary>
+    /// <param name="sortedArray">Source array.</param>
+    /// <returns>Array without repeated elements.</returns>
+    public double[] GetSortedArrayWhithoutRepeat(double[] array)
+    {
+      List<double> result = sorter.MergeSort(array.ToList<double>());
+      if (array.Length == 1)
+      {
+        return array;
+      }
+
+      for (int i = 0; i < result.Count - 1; i++)
+      {
+        if (this.CompareDoubles(result[i], result[i + 1]))
+        {
+          result.Remove(result[i + 1]);
+        }
+      }
+      return result.ToArray<double>();
+    }
+
+    /// <summary>
+    /// Return the result of compare to double values.
+    /// </summary>
+    /// <param name="x">First value</param>
+    /// <param name="y">Second value</param>
+    /// <returns>True if x equals to y;false otherwise</returns>
+    public bool CompareDoubles(double x, double y)
+    {
+      double eps = Math.Min(x, y) * 10e-20;
+      if (Math.Abs(x - y) < eps)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+  }
+}
---
+++ DEV-10/Sorter.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DEV_10
+{
+
+  /// <summary>
+  /// Sorts arrays.
+  /// </summary>
+  class Sorter
+  {
+
+    /// <summary>
+    /// Bubble sort.
+    /// </summary>
+    /// <param name="array">Array to sort</param>
+    /// <returns>Sorted array.</returns>
+    public double[] BubbleSort(double[] array)
+    {
+      double value = 0;
+      bool count = true;
+      while (count)
+      {
+        count = false;
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+          if (array[i] > array[i + 1])
+          {
+            value = array[i];
+            array[i] = array[i + 1];
+            array[i + 1] = value;
+            count = true;
+          }
+        }
+      }
+      return array;
+    }
+
+    /// <summary>
+    /// Merge sort.
+    /// </summary>
+    /// <param name="array">Array to sort.</param>
+    /// <returns>Sorted array.</returns>
+    public List<double> MergeSort(List<double> array)
+    {
+      if (array.Count == 1)
+      {
+        return array;
+      }
+      List<double> left = new List<double>();
+      List<double> right = new List<double>();
+      int middle = array.Count / 2;
+      for (int i = 0; i < middle; i++)
+      {
+        left.Add(array[i]);
+      }
+      for (int i = middle; i < array.Count; i++)
+      {
+        right.Add(array[i]);
+      }
+      return Merge(MergeSort(left), MergeSort(right));
+    }
+
+    private List<double> Merge(List<double> left, List<double> right)
+    {
+      int leftCount = 0;
+      int rightCount = 0;
+      List<double> result = new List<double>();
+      while (leftCount < left.Count && rightCount < right.Count)
+      {
+        if (left[leftCount].CompareTo(right[rightCount]) < 0)
+        {
+          result.Add(left[leftCount]);
+          leftCount++;
+        }
+        else
+        {
+          result.Add(right[rightCount]);
+          rightCount++;
+        }
+      }
+      if (right.Count > rightCount)
+      {
+        for (int i = rightCount; i < right.Count; i++)
+        {
+          result.Add(right[i]);
+        }
+      }
+      else
+      {
+        for (int i = leftCount; i < left.Count; i++)
+        {
+          result.Add(left[i]);
+        }
+      }
+      return result;
+    }
+  }
+}
