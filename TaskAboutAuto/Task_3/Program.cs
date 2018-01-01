fec5e2172215bd07f1ca55e69f53c2337cab0550
ButurlyaAL <stdlibh1421@gmail.com>
2018-01-02 01:15:26 +0200

    taskAboutAuto

---
+++ TaskAboutAuto/Task_3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task_3
+{
+  /// <summary>
+  /// Entry point.
+  /// </summary>
+  class Program
+  {     
+    static void Main(string[] args)
+    {
+      List<Auto> listOfAuto = new List<Auto>();
+      DataProvider provider = new DataProvider();   
+      provider.EnterDataToList(listOfAuto);
+      listOfAuto.Sort();
+      foreach (Auto auto in listOfAuto)
+      {
+        Console.WriteLine(auto.ToString());
+      }
+    }
+  }
+}
