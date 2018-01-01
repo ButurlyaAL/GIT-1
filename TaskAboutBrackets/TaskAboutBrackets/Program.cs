b6e8e21c86a76dcedbad3d54c9ef32384ed80e25
ButurlyaAL <stdlibh1421@gmail.com>
2018-01-02 00:59:58 +0200

    taskAboutBrackets

---
+++ TaskAboutBrackets/TaskAboutBrackets/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TaskAboutBrackets
+{
+  /// <summary>
+  /// Entry point.
+  /// </summary>
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      StringValidator validator = new StringValidator();
+      Console.WriteLine("Enter an expression :");
+      Console.WriteLine(validator.StringIsValid(Console.ReadLine()));
+    }
+  }
+}
