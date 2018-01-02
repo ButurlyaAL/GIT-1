7ff5f057b00705f3d3aad109255c12fa3ea36c29
ButurlyaAL <stdlibh1421@gmail.com>
2018-01-02 02:30:16 +0200

    added PostPageTest and added comments to PostPage.cs

--- WordPress/WebTests/PostPage.cs
+++ WordPress/WebTests/PostPage.cs
@@ -3,6 +3,10 @@ using OpenQA.Selenium;
 
 namespace WebTests
 {
+
+  /// <summary>
+  /// Representation of post page.
+  /// </summary>
   class PostPage : PageBase
   {
     private IWebDriver webDriver;
@@ -25,41 +29,65 @@ namespace WebTests
     [FindsBy(How = How.XPath, Using = "//input[@value = 'Post Comment']")]
     public IWebElement postCommentButton { get; set; }
 
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="webDriver">Instance of WebDriver</param>
     public PostPage(IWebDriver webDriver) : base(webDriver)
     {
       this.webDriver = webDriver;
       PageFactory.InitElements(this.webDriver, this);
     }
 
+    /// <summary>
+    /// Clicks button previous post.
+    /// </summary>
     public void ClickPreviousPostButton()
     {
       previousPostButton.Click();
     }
 
+    /// <summary>
+    /// Clicks button next post.
+    /// </summary>
     public void ClickNextPostButton()
     {
       nextPostButton.Click();
     }
 
+    /// <summary>
+    /// Types comment content.
+    /// </summary>
+    /// <param name="comment">Comment content.</param>
     public void TypeComment(string comment)
     {
       commentTextField.SendKeys(comment);
     }
 
+    /// <summary>
+    /// Types name to nameTextField.
+    /// </summary>
+    /// <param name="name">Name to type.</param>
     public void TypeName(string name)
     {
       nameTextField.SendKeys(name);
     }
 
+    /// <summary>
+    /// Types email to emailTextField.
+    /// </summary>
+    /// <param name="email">Email to type.</param>
     public void TypeEmail(string email)
     {
       emailTextField.SendKeys(email);
     }
 
+    /// <summary>
+    /// Clicks post comment button.
+    /// </summary>
     public void ClickPostCommentButton()
     {
       postCommentButton.Click();
     }
-
   }
 }
---
+++ WordPress/WebTests/PostPageTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;];
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace WebTests
+{
+  /// <summary>
+  /// Test of landing page of the site.
+  /// </summary>
+  class PostPageTests : TestBase
+  {
+    private IWebDriver webDriver;
+    private LoginPage loginPage;
+
+    [Test]
+    public void TestPreviousPostsButtonPositive()
+    {
+      
+    }
+
+    [Test]
+    public void TestNextPostsButtonPositive()
+    {
+
+    }
+
+    [SetUp]
+    public void beforeTest()
+    {
+      
+    }
+  }
+}
